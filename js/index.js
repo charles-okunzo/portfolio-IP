078dbd828da7d85f3f6bb4145a5c0aa937d39fa4
charles okunzo <okunzopeter01@gmail.com>
2022-02-26 16:28:44 +0300

    feat:Scroll top button- enabled fuctionality of scroll to top button

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ portfolioBtn.onclick=()=>{
   var portfolio=document.getElementById("portfolio");
 
   portfolio.scrollIntoView();
-}
+};
 
 const aboutBtn=document.getElementById("aboutBtn");
 
@@ -28,7 +28,7 @@ aboutBtn.onclick=()=>{
   var about=document.getElementById("about");
 
   about.scrollIntoView();
-}
+};
 
 const contactBtn=document.getElementById("contactBtn");
 
@@ -36,4 +36,21 @@ contactBtn.onclick= ()=>{
   contact=document.getElementById("contact");
 
   contact.scrollIntoView();
-}
+};
+
+const scrollBtn=document.getElementById("scrollBtn");
+
+const goToTop=document.body.scrollTop;
+
+window.onscroll=()=>{
+  if(goToTop>100 || document.documentElement.scrollTop>100){
+    scrollBtn.style.display="block";
+  } else{
+    scrollBtn.style.display="none";
+  }
+};
+
+scrollBtn.onclick=()=>{
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;
+};
